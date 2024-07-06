84b9571f3b4bec01de2233f3e6e94848ee3fdff8
basnetnarayan <basnetnarayan57972@gmail.com>
2024-07-06 07:58:42 +0545

    Nice

--- Architect/img-change.js
+++ Architect/img-change.js
@@ -1,8 +1,9 @@
  // Array of background images
  const images = [
+     'url("https://bwvision.com/wp-content/uploads/2023/02/Doha-Skyline-with-Burj-Doha-no-border.jpg")',
     'url("https://images.pexels.com/photos/135018/pexels-photo-135018.jpeg?cs=srgb&dl=pexels-joshkjack-135018.jpg&fm=jpg")',
-    'url("https://bwvision.com/wp-content/uploads/2023/02/Doha-Skyline-with-Burj-Doha-no-border.jpg")',
     'url("https://www.londonfineartphotography.co.uk/wp-content/uploads/2018/09/Building.jpg")'
+    // Add more image URLs as needed
 ];
 
 let currentIndex = 0;
@@ -26,3 +27,41 @@ document.getElementById('next').addEventListener('click', () => {
 
 // Initialize with the first image
 updateBackgroundImage(currentIndex);
+
+//for bg-color
+let isBlack = false;
+
+function setColor() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.querySelector('.text-box').style.color = 'black';
+    document.querySelector('.swap').style.color = 'black';
+    document.getElementById('nav-list1').style.color = 'white';
+    document.getElementById('nav-list2').style.color = 'white';
+    document.getElementById('nav-list3').style.color = 'white';
+    document.getElementById('nav-list4').style.color = 'white';
+    document.getElementById('nav-list5').style.color = 'white';
+    document.querySelector('.text-over-img').style.borderColor='white';
+}
+
+function resetColor() {
+    document.body.style.backgroundColor = '';
+    document.body.style.color = '';
+    document.querySelector('.text-box').style.color = '';
+    document.querySelector('.swap').style.color = '';
+    document.getElementById('nav-list1').style.color = '';
+    document.getElementById('nav-list2').style.color = '';
+    document.getElementById('nav-list3').style.color = '';
+    document.getElementById('nav-list4').style.color = '';
+    document.getElementById('nav-list5').style.color = '';
+    document.querySelector('.text-over-img').style.borderColor='   ';
+}
+
+document.getElementById('bg-color').addEventListener('click', () => {
+    if (isBlack) {
+        resetColor();
+    } else {
+        setColor();
+    }
+    isBlack = !isBlack;
+});
